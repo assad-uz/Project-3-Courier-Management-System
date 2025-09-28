4114535a3a5cc09d82396baa38624467ae46a91e
Assaduzzaman Shaon <assad.uz255@gmail.com>
2025-09-28 10:03:54 +0600

    Update

--- cms/src/App.jsx
+++ cms/src/App.jsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Master from "./pages/admin/Master";
 
 const App = () => {
   return (
     <div>
-      Hi
+      <div>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Master />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
     </div>
   );
 };
